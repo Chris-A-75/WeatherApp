c36b7f6ef2b4a7863bc16909b74582bee5ba6b43
Chris_A_75 <chrisaccaoui@protonmail.com>
2022-05-31 22:16:45 +0300

    Fixed design and value displaying

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
 import * as Location from 'expo-location';
 
 export default function App() {
@@ -18,7 +18,6 @@ export default function App() {
   }, []);
 
   const openWeatherKey = `75f7da7b194ffe59242dea1aafd6771a`;
-  var fetched;
   
   fetch(`https://api.openweathermap.org/data/2.5/onecall?&units=metric&exclude=minutely&appid=${openWeatherKey}&lat=33.8478564&lon=35.5491985`)
     .then(response => {
@@ -26,24 +25,24 @@ export default function App() {
     })
     .then(data => {
       fetchedTemp = data.current.temp;
-      fetchedTemp = parseInt(fetchedTemp + 1);
+      fetchedTemp = parseInt(fetchedTemp);
       setTemp(fetchedTemp);
       fetchedMain = data.current.weather[0].main;
-      setMain(fetchedMain);
+      fetchedMainDesc = data.current.weather[0].description;
+      setMain(fetchedMainDesc);
       fetchedLike = data.current.feels_like;
-      fetchedLike = parseInt(fetchedLike + 1);
-      setLike(fetchedLike);;
+      fetchedLike = parseInt(fetchedLike);
+      setLike(fetchedLike);
       fetchedHumid = data.current.humidity;
       setHumidity(fetchedHumid);
     })
   return (
     <View style={styles.container}>
       <Text style={styles.main}>{displayTemp}°</Text>
-      <Text style={styles.paragraph}>The weather is {displayMain}</Text>
+      <Text style={styles.submain}>{displayMain}</Text>
       <Text style={styles.paragraph}>Feels like: {displayLike}°</Text>
       <Text style={styles.paragraph}>Humidity: {displayHumidity}%</Text>
         
-
   </View>
   );
 }
@@ -62,5 +61,12 @@ const styles = StyleSheet.create({
     fontSize: 90,
     textAlign: 'center',
     fontWeight: 'bold',
+    marginLeft: 50,
+  },
+  submain: {
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginBottom: 100,
   }
 })

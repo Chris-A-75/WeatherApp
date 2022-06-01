74d5e5a3d5d0960b2f28d4b14dbbfd92d394099b
Chris_A_75 <chrisaccaoui@protonmail.com>
2022-06-01 21:08:06 +0300

    fixed design, added info

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, ScrollView, StyleSheet, SafeAreaView, Animated } from 'react-native';
 import * as Location from 'expo-location';
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [displayTemp, setTemp] = useState('20');
   const [displayMain, setMain] = useState('Clear');
-  const [displayLike, setLike] = useState('20');
+  const [displayVis, setVis] = useState('10');
   const [displayHumidity, setHumidity] = useState('50');
+  const [displayPressure, setPressure] = useState('1000');
+  const [displayDew, setDew] = useState('15');
+  const [displaySunset, setSunset] = useState('5:00');
+  const [displaySunrise, setSunrise] = useState('5:00');
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
 
   useEffect(() => {
     (async () => {
@@ -16,9 +23,28 @@ export default function App() {
       setLocation(location);
     })();
   }, []);
+  
+  const fadeIn = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  getCurrentTimeFromStamp = function(timestamp,sunSet) {
+    var d = new Date(timestamp);
+    if (sunSet){
+      timeStampCon = (d.getHours() + 2) + ':' + d.getMinutes();
+    } 
+    else{
+      timeStampCon = (d.getHours()) + ':' + d.getMinutes();
+    }
+    return timeStampCon;
+};
 
   const openWeatherKey = `75f7da7b194ffe59242dea1aafd6771a`;
-  
+  fadeIn();
   fetch(`https://api.openweathermap.org/data/2.5/onecall?&units=metric&exclude=minutely&appid=${openWeatherKey}&lat=33.8478564&lon=35.5491985`)
     .then(response => {
       return response.json();
@@ -27,24 +53,45 @@ export default function App() {
       fetchedTemp = data.current.temp;
       fetchedTemp = parseInt(fetchedTemp);
       setTemp(fetchedTemp);
+
       fetchedMain = data.current.weather[0].main;
       fetchedMainDesc = data.current.weather[0].description;
       setMain(fetchedMainDesc);
-      fetchedLike = data.current.feels_like;
-      fetchedLike = parseInt(fetchedLike);
-      setLike(fetchedLike);
+
+      fetchedVis = data.current.visibility / 1000;
+      setVis(fetchedVis);
+
       fetchedHumid = data.current.humidity;
       setHumidity(fetchedHumid);
+
+      fetchedPressure = data.current.pressure;
+      setPressure(fetchedPressure);
+
+      fetchedDew = data.current.dew_point;
+      setDew(parseInt(fetchedDew));
+
+      fetchedSunset = data.current.sunset;
+      setSunset(getCurrentTimeFromStamp(fetchedSunset,true));
+
+      fetchedSunrise = data.current.sunrise;
+      setSunrise(getCurrentTimeFromStamp(fetchedSunrise,false));
     })
   return (
-    <View style={styles.container}>
-      <Text style={styles={textAlign: 'left'}}>Today</Text>
+    <ScrollView style={styles.container}>
+      <Text style={{textAlign: 'center', fontSize: 25, fontWeight: 'bold', marginTop: 70, marginBottom: 50}}>Today</Text>
       <Text style={styles.main}>{displayTemp}°</Text>
       <Text style={styles.submain}>{displayMain}</Text>
-      <Text style={styles.paragraph}>Feels like: {displayLike}°</Text>
-      <Text style={styles.paragraph}>Humidity: {displayHumidity}%</Text>
-        
-  </View>
+
+      <Animated.View style={[styles.container2,{opacity: fadeAnim}]}>
+        <Text style={styles.paragraph}>Sunrise: {displaySunrise} AM</Text>  
+        <Text style={styles.paragraph}>Sunset: {displaySunset} PM</Text>
+        <Text style={styles.paragraph}>Visibility: {displayVis} km</Text>
+        <Text style={styles.paragraph}>Humidity: {displayHumidity}%</Text>
+        <Text style={styles.paragraph}>Pressure: {displayPressure} mb</Text>
+        <Text style={styles.paragraph}>Dew point: {displayDew}°C</Text>
+      </Animated.View>
+
+  </ScrollView>
   );
 }
 
@@ -52,11 +99,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'skyblue',
-    paddingTop: 100,
+    //paddingTop: 100,
+  },
+  container2: {
+    backgroundColor: '#55c2ed',
+    marginLeft: 10,
+    marginRight: 10,
+    padding: 20,
+    borderRadius: 10,
   },
   paragraph: {
-    fontSize: 18,
-    textAlign: 'center',
+    fontSize: 20,
+    textAlign: 'left',
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
   main: {
     fontSize: 90,
